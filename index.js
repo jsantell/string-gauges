f531566e8e9afd65ab3131f28d0de441e064d85c
Jordan Santell <jordan@jsantell.com>
2016-08-26 11:11:58 -0700

    Move types to their own definitions

--- index.js
+++ index.js
@@ -1,53 +1,7 @@
-var types = {
-  'daddario-pl': {
-    brand: 'D\'Addario',
-    type: 'plain',
-    name: 'Plain Steel - Lock Twist',
-  },
-  'daddario-nw': {
-    brand: 'D\'Addario',
-    type: 'wound',
-    name: 'XL - Nickelplated Steel Round Wound',
-  },
-  'kalium-plain': {
-    brand: 'Kalium',
-    type: 'plain',
-    name: 'Plain',
-  },
-  'kalium-wound': {
-    brand: 'Kalium',
-    type: 'wound',
-    name: 'Wound',
-  },
-  'elixer-nanoweb': {
-    brand: 'Elixer',
-    type: '',
-    name: 'Electric NANOWEB',
-  },
-  'elixer-polyweb': {
-    brand: 'Elixer',
-    type: '',
-    name: 'Electric POLYWEB',
-  },
-  'elixer-acoustic-phosphor-nanoweb': {
-    brand: 'Elixer',
-    type: '',
-    name: 'Acoustic Phosphor Bronze NANOWEB',
-  },
-  'elixer-acoustic-80-nanoweb': {
-    brand: 'Elixer',
-    type: '',
-    name: 'Acoustic 80/20 Bronze NANOWEB',
-  },
-  'elixer-acoustic-80-polyweb': {
-    brand: 'Elixer',
-    type: '',
-    name: 'Acoustic 80/20 Bronze POLYWEB',
-  },
-};
+var TYPES = require('./types.json');
 
-Object.keys(types).forEach(function (type) {
-  types[type].data = require('./data/' + type + '.json');
-});
-
-module.exports = types;
+module.exports = Object.keys(TYPES).reduce(function (exporter, type) {
+  exporter[type] = TYPES[type];
+  exporter[type].data = require('./data/' + type + '.json');
+  return exporter;
+}, Object.create(null));
